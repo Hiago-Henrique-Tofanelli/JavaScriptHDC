b78db74bfb231d22dc6a01933a2f4a38cae9c17d
Hiago-Henrique-Tofanelli <hiagotofanelli@hotmail.com>
2023-07-26 23:46:08 -0300

    Primeiro dia

---
+++ 4_variaveis/js/scripts.js
@@ -0,0 +1,41 @@
+var teste = 1;
+
+console.log(teste);
+
+teste = 'Matheus';
+
+console.log(teste);
+
+/* variáveis não podem ser declaradas com
+ caracteres especiais
+
+ Exemplo:
+
+ var @nome = 'ASD';
+ console.log(nome);
+*/
+var nome5 = 'ASD3';
+
+console.log(nome5);
+
+var meuPrimeiroNome = "Hiago"; // camelCase
+
+console.log(meuPrimeiroNome);
+
+var meuSobrenome = "Tofanelli"
+
+console.log(meuSobrenome);
+
+let testando = 1;
+const ola = 2;
+
+console.log(testando);
+console.log(ola);
+
+var meuNome;
+
+console.log(meuNome);
+
+meuNome = "Hiago";
+
+console.log(meuNome);
