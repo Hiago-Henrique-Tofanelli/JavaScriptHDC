e5bf392e4c100355a4866a58b543538f27f0ef86
Hiago-Henrique-Tofanelli <hiagotofanelli@hotmail.com>
2023-07-26 23:29:57 -0300

    Primeiro dia

---
+++ 3_particularidadesjavascript/js/script.js
@@ -0,0 +1,29 @@
+console.log("Primeiro");
+
+console.log("Último");
+
+console.log("Mais um console.log");
+
+// Declara uma variável
+/* JavaScript é uma linguagem fracamente tipada,
+pode-se notar isso com a variável "a" que não assume um 
+tipo de valor e pode ser modificada durante o código.
+*/
+
+var a = 1;
+
+a = 'teste';
+
+console.log(a);
+
+// Este é um comentário
+
+/*
+Este 
+é
+um
+comentário 
+de 
+múltiplas 
+linhas
+*/
