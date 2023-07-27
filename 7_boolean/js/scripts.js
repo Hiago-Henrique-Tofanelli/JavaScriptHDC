1258bad002982b87f0abf78511389f1b3228823d
Hiago-Henrique-Tofanelli <hiagotofanelli@hotmail.com>
2023-07-27 18:48:20 -0300

    segundo dia

---
+++ 7_boolean/js/scripts.js
@@ -0,0 +1,24 @@
+//boolean representa um valor verdadeiro ou falso
+
+var verdadeiro = true;
+
+console.log(verdadeiro);
+console.log(typeof verdadeiro);
+
+console.log(typeof true);
+console.log(typeof false);
+
+var falso = false;
+
+console.log(falso);
+console.log(typeof falso);
+/*
+Alguns nomes não podem ser usados
+para delacarar variáveis
+
+Exemplos:
+var false = 3;
+var true= 3;
+var var = 3;
+
+*/
